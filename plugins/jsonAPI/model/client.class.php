e49225efe7b03dc13d39e3afa5825e11caa05e0c
Mike Joseph <mike.joseph5@gmail.com>
2011-04-21 16:32:17 -0400

    client model and controller (renamed from advertiser)

---
+++ plugins/jsonAPI/model/client.class.php
@@ -0,0 +1,19 @@
+<?php
+namespace jsonAPI\model;
+
+class client extends \jsonAPI\model {
+	
+	public function __init() {
+
+        $this->stack['advertiserName'] = $this->stack['clientname'];
+        $this->stack['agencyName'] = $this->stack['name'];
+        $this->stack['contactName'] = $this->stack['contact'];
+        $this->stack['emailAddress'] = $this->stack['email'];
+        $this->stack['agencyId'] = $this->stack['agencyid'];
+        $this->stack['advertiserId'] = $this->stack['clientid'];
+        $this->stack['accountId'] = $this->stack['account_id'];
+
+	}
+	
+}
+?>
